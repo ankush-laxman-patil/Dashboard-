1be5c3006f37ea01f20a898c9da17a2e871b5674
ankush-laxman-patil <68821491+ankush-laxman-patil@users.noreply.github.com>
2020-07-30 17:46:33 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,33 +1,25 @@
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
-print(dcc.__version__) # 0.6.0 or above is required
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+from app import app
+from layouts import layout1, layout2
+import callbacks
 
 app.layout = html.Div([
-    # represents the URL bar, doesn't render anything
     dcc.Location(id='url', refresh=False),
-
-    dcc.Link('Navigate to "/"', href='/'),
-    html.Br(),
-    dcc.Link('Navigate to "/page-2"', href='/page-2'),
-
-    # content will be rendered in this element
     html.Div(id='page-content')
 ])
 
-
-@app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
 def display_page(pathname):
-    return html.Div([
-        html.H3('You are on page {}'.format(pathname))
-    ])
-
+    if pathname == '/apps/app1':
+         return layout1
+    elif pathname == '/apps/app2':
+         return layout2
+    else:
+        return layout2
 
 if __name__ == '__main__':
     app.run_server(debug=True)
